a8c273b9a82c1a2c1f4fc57762d241eed122ff1d
Abdi <abdi.haji1716@gmail.com>
2023-03-11 09:07:12 +0000

    Capstone Project Code

---
+++ src/main/java/tek/sdet/framework/config/ChromeBrowser.java
@@ -0,0 +1,20 @@
+package tek.sdet.framework.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ChromeBrowser implements Browser {
+
+
+	public WebDriver OpenBrowser(String url) {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions ops = new ChromeOptions();
+        ops.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(ops);
+		return driver;
+        }
+
+}
